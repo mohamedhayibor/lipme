9d90a3f1f9b801f3b41fb7e86d6a113ae44a05d7
Mohamed Hayibor <mohamedhayibor@gmail.com>
2016-06-19 13:14:37 -0400

    ship it

---
+++ index.js
@@ -0,0 +1,35 @@
+'use strict';
+
+const express = require('express');
+const ejs = require('ejs');
+const path = require('path');
+const app = express();
+
+const async = require("async");
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+// homepage - landing page
+app.get('/', (req, res) => {
+  res.render('landing');
+});
+
+const lip = require('./lip.json');
+
+// listing
+app.get('/listing', (req, res) => {
+  res.render('listing', { lip: lip });
+})
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, (err) => {
+
+  if (err) {
+    throw new Error(err);
+  }
+
+  console.log('serving at: ', PORT);
+})
---
+++ public/js/capture.js
@@ -0,0 +1,149 @@
+/* Source code from: https://raw.githubusercontent.com/mdn/samples-server/master/s/webrtc-capturestill/capture.js
+
+Source of article: https://developer.mozilla.org/en-US/docs/Web/API/WebRTC_API/Taking_still_photos
+*/
+(function() {
+  // The width and height of the captured photo. We will set the
+  // width to the value defined here, but the height will be
+  // calculated based on the aspect ratio of the input stream.
+
+  var width = 320;    // We will scale the photo width to this
+  var height = 0;     // This will be computed based on the input stream
+
+  // |streaming| indicates whether or not we're currently streaming
+  // video from the camera. Obviously, we start at false.
+
+  var streaming = false;
+
+  // The various HTML elements we need to configure or control. These
+  // will be set by the startup() function.
+
+  var video = null;
+  var canvas = null;
+  var photo = null;
+  var startbutton = null;
+
+  function startup() {
+    video = document.getElementById('video');
+    canvas = document.getElementById('canvas');
+    photo = document.getElementById('photo');
+    startbutton = document.getElementById('startbutton');
+
+    navigator.getMedia = ( navigator.getUserMedia ||
+                           navigator.webkitGetUserMedia ||
+                           navigator.mozGetUserMedia ||
+                           navigator.msGetUserMedia);
+
+    navigator.getMedia(
+      {
+        video: true,
+        audio: false
+      },
+      function(stream) {
+        if (navigator.mozGetUserMedia) {
+          video.mozSrcObject = stream;
+        } else {
+          var vendorURL = window.URL || window.webkitURL;
+          video.src = vendorURL.createObjectURL(stream);
+        }
+        video.play();
+      },
+      function(err) {
+        console.log("An error occured! " + err);
+      }
+    );
+
+    video.addEventListener('canplay', function(ev){
+      if (!streaming) {
+        height = video.videoHeight / (video.videoWidth/width);
+
+        // Firefox currently has a bug where the height can't be read from
+        // the video, so we will make assumptions if this happens.
+
+        if (isNaN(height)) {
+          height = width / (4/3);
+        }
+
+        video.setAttribute('width', width);
+        video.setAttribute('height', height);
+        canvas.setAttribute('width', width);
+        canvas.setAttribute('height', height);
+        streaming = true;
+      }
+    }, false);
+
+    startbutton.addEventListener('click', function(ev){
+      takepicture();
+      ev.preventDefault();
+    }, false);
+
+    clearphoto();
+  }
+
+  // Fill the photo with an indication that none has been
+  // captured.
+
+  function clearphoto() {
+    var context = canvas.getContext('2d');
+    context.fillStyle = "#AAA";
+    context.fillRect(0, 0, canvas.width, canvas.height);
+
+    var data = canvas.toDataURL('image/png');
+    photo.setAttribute('src', data);
+  }
+
+  // Capture a photo by fetching the current contents of the video
+  // and drawing it into a canvas, then converting that to a PNG
+  // format data URL. By drawing it on an offscreen canvas and then
+  // drawing that to the screen, we can change its size and/or apply
+  // other changes before drawing it.
+
+
+  // grey:
+
+  function takepicture() {
+    var context = canvas.getContext('2d');
+    if (width && height) {
+      canvas.width = width;
+      canvas.height = height;
+      context.drawImage(video, 0, 0, width, height);
+
+      console.log('video: ', video)
+
+      //var neededImage =
+
+      var data = canvas.toDataURL('image/png');
+
+      console.log('canvas: ', canvas );
+
+      var getRGB = function () {
+        var imgd = context.getImageData( 2/5 * width, height * 2/5, 3/5 *width, 3/5 * height);
+        var pix = imgd.data;
+
+        var red = 0, green = 0, blue = 0;
+
+        // Loop over each pixel and invert the color.
+        for (var i = 0, n = pix.length; i < n; i += 4) {
+          red += pix[i  ]; // red
+          green += pix[i+1]; // green
+          blue += pix[i+2]; // blue
+          // i+3 is alpha (the fourth element)
+        }
+        red = red / (pix.length / 4);
+        green = green / (pix.length / 4);
+        blue = blue / (pix.length / 4);
+
+        return [red, green, blue];
+
+        window.location.href = "http://litme.herokuapp.com";
+      };
+      // photo.setAttribute('src', data);
+    } else {
+      clearphoto();
+    }
+  }
+
+  // Set up our event listener to run the startup process
+  // once loading is complete.
+  window.addEventListener('load', startup, false);
+})();
